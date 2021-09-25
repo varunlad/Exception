e48fffafed5e85fe378efdcc9fc7b2de23e8930f
varun <varunlad5@gmail.com>
2021-09-25 19:29:10 +0530

    [varun] UC2 Passing Null Messsage to call Exception

--- MoodTesting/UnitTest1.cs
+++ MoodTesting/UnitTest1.cs
@@ -3,12 +3,58 @@ using System;
 
 namespace MoodTesting
 {
-    [TestClass]
+  [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GivenHappyShouldReturnHappy()
         {
+            //AAA Methology
+
+            //Arrange
+            string excepted = "happy";
+            ExceptionMood moodAnalyser = new ExceptionMood("I am in a happy Mood");//Creating a object and passing a message
+
+            //ACT
+            string actual = moodAnalyser.AnalyzeMood();
+            
+                //ASSERT
+                Assert.AreEqual(excepted, actual); ;//Checking wether my actual rasult and Excepted Results Matches or not
+
         }
+
+        [TestMethod]
+        public void GivenSadShouldReturnSad()
+        {
+            //AAA Methology
+
+            //Arrange
+            string excepted = "sad";
+            ExceptionMood moodAnalyser = new ExceptionMood("I am in a sad mood");//Creating a object and passing a message
+
+            //ACT
+            string actual = moodAnalyser.AnalyzeMood();
+
+            //ASSERT
+            Assert.AreEqual(excepted, actual);//Checking wether my actual rasult and Excepted Results Matches or not
+
+        }
+        [TestMethod]
+        public void GivenNullShouldReturnHappy()
+        {
+            //AAA Methology
+
+            //Arrange
+            string excepted = "happy";
+            ExceptionMood moodAnalyser = new ExceptionMood(null);//Creating a object and passing a message
+
+            //ACT
+            string actual = moodAnalyser.AnalyzeMood();
+
+            //ASSERT
+            Assert.AreEqual(excepted, actual);//Checking wether my actual rasult and Excepted Results Matches or not
+
+        }
+
     }
 }
