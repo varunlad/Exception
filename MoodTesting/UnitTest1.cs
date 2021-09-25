1ac50ec3560ab845153b5fe8ffe4244580f482ca
varun <varunlad5@gmail.com>
2021-09-25 19:23:41 +0530

    [varun] UC1 Analyze the Mood

--- MoodTesting/UnitTest1.cs
+++ MoodTesting/UnitTest1.cs
@@ -7,8 +7,37 @@ namespace MoodTesting
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GivenHappyShouldReturnHappy()
         {
+            //AAA Methology
+
+            //Arrange
+            string excepted = "happy";
+            ExceptionMood moodAnalyser = new ExceptionMood("I am in a happy Mood");//Creating a object and passing a message
+
+            //ACT
+            string actual = moodAnalyser.AnalyzeMood();
+
+            //ASSERT
+            Assert.AreEqual(excepted, actual); ;//Checking wether my actual rasult and Excepted Results Matches or not
+
+        }
+
+        [TestMethod]
+        public void GivenSadShouldReturnSad()
+        {
+            //AAA Methology
+
+            //Arrange
+            string excepted = "sad";
+            ExceptionMood moodAnalyser = new ExceptionMood("I am in a sad mood");//Creating a object and passing a message
+
+            //ACT
+            string actual = moodAnalyser.AnalyzeMood();
+
+            //ASSERT
+            Assert.AreEqual(excepted, actual);//Checking wether my actual rasult and Excepted Results Matches or not
+
         }
     }
 }
