fad5f8ecdc64239c4a8221f2d76235c1380b7e28
varun <varunlad5@gmail.com>
2021-09-29 17:26:08 +0530

    [varun] Reflection in Mood Analyzer

---
+++ Exception058/MoodAnalyser.cs
@@ -0,0 +1,54 @@
+﻿using MoodAnlyzesUnitTesting;
+using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception058
+{
+    public class MoodAnalyzer
+    {
+        public string message;
+
+        public MoodAnalyzer(string message)
+        {
+            this.message = message;
+        }
+        public MoodAnalyzer()
+        {
+            Console.WriteLine("Default Constructor");
+        }
+
+        public string AnalyzeMood()
+        {
+            try
+            {
+                if (message.ToLower().Equals(string.Empty))
+                {
+                    throw new CustomMoodAnalyzerException(CustomMoodAnalyzerException.ExceptionType.EMPTY_TYPE_EXCEPTION, "Message should not be empty");
+                }
+                else if (message.ToLower().Contains("sad"))
+                {
+                    return "sad";
+                }
+                else
+                {
+                    return "happy";
+                }
+            }
+            catch (NullReferenceException)
+            {
+                throw new CustomMoodAnalyzerException(CustomMoodAnalyzerException.ExceptionType.INVALID_MOOD_EXCEPTION, "Message should not be null");
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+
+        }
+    }
+}
+
+
---
+++ MoodAnlyzesUnitTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MoodAnlyzesUnitTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("MoodAnlyzesUnitTesting")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6c8cf23e-3093-416d-9cfc-d24962e57b0f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
